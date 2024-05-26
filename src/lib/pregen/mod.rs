ab9810ae56a691ca2c8bfca631e52c0af4c21589
Warren Spits <warren@advantic.au>
2024-05-26 20:36:59 +1000

    Platform specific bindings.rs
    Closes #6

--- src/lib/pregen/mod.rs
+++ src/lib/pregen/mod.rs
@@ -1,5 +1,8 @@
 #![allow(clippy::all)]
 #![allow(clippy::pedantic)]
 #![allow(clippy::nursery)]
+
+#[cfg_attr(all(target_os="windows", target_arch="x86_64"), path = "x86_64-windows-bindings.rs")]
+#[cfg_attr(all(target_os="linux",   target_arch="x86_64"), path = "x86_64-linux-bindings.rs")]
 mod bindings;
 pub use bindings::*;
