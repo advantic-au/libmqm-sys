611001a3c00f2dfef620cdf62848e594bbbfd2ec
Warren Spits <warren@advantic.au>
2024-06-11 20:54:58 +1000

    MacOS support. Closes #1

--- src/lib/pregen/mod.rs
+++ src/lib/pregen/mod.rs
@@ -4,5 +4,6 @@
 
 #[cfg_attr(all(target_os="windows", target_arch="x86_64"), path = "x86_64-windows-bindings.rs")]
 #[cfg_attr(all(target_os="linux",   target_arch="x86_64"), path = "x86_64-linux-bindings.rs")]
+#[cfg_attr(target_os="macos", path = "any-macos-bindings.rs")]
 mod bindings;
 pub use bindings::*;
