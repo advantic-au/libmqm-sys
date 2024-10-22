72dc5c180424a6e3550093d072493e60eb3feff5
Warren Spits <warren@advantic.au>
2024-10-22 17:42:25 +1100

    MQ client 9.2 (#12)
    MQ client 9.2 - 9.4 support

--- build/mod.rs
+++ build/mod.rs
@@ -30,7 +30,7 @@ mod features {
 
 #[cfg(feature = "mqi_helpers")]
 mod mqi_helpers {
-    use std::{io, panic::catch_unwind, path::PathBuf};
+    use std::{io, path::PathBuf};
 
     /// Source files that are built by cc to support the bindings
     const SOURCE_FILES: &[super::features::FeatureFilter<&str>] = &[
@@ -44,16 +44,33 @@ mod mqi_helpers {
         for source in &sources {
             println!("cargo:rerun-if-changed={source}")
         }
-        catch_unwind(|| {
-            cc::Build::new()
-                .static_flag(false)
-                .flag_if_supported("-nostartfiles")
-                .include(mq_inc_path)
-                .files(sources)
-                .warnings(true)
-                .compile("mq_functions")
-        })
-        .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "Failed to compile c files"))
+
+        cc::Build::new()
+            .static_flag(false)
+            .flag_if_supported("-nostartfiles")
+            .include(mq_inc_path)
+            .files(sources)
+            .warnings(true)
+            .try_compile("mqi_helpers")
+            .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "Failed to compile c files"))
+    }
+}
+
+mod support {
+    pub const CFG_CHECKS: &[(&str, &str)] = &[("mqi_mqwqr4", "./build/c/mqwqr4.c"), ("mqi_mqbno", "./build/c/mqbno.c")];
+
+    #[cfg(feature = "bindgen")]
+    pub fn check_compile(cfg: &str, src: &str, mq_inc_path: &std::path::PathBuf) -> Result<(), cc::Error> {
+        cc::Build::new()
+            .static_flag(false)
+            .flag_if_supported("-nostartfiles")
+            .include(mq_inc_path)
+            .file(src)
+            .cargo_output(false)
+            .cargo_warnings(false)
+            .cargo_metadata(false)
+            .cargo_debug(false)
+            .try_compile(cfg)
     }
 }
 
@@ -114,18 +131,38 @@ fn main() -> Result<(), io::Error> {
     #[cfg(feature = "mqi_helpers")]
     mqi_helpers::build_c(&mq_path::mq_inc_path())?; // Build the c files
 
+    for &(cfg, _) in support::CFG_CHECKS {
+        println!("cargo:rustc-check-cfg=cfg({cfg})");
+        #[cfg(not(feature = "bindgen"))]
+        println!("cargo:rustc-cfg={cfg}");
+    }
+
     #[cfg(feature = "bindgen")]
     {
         use regex_lite::Regex;
         use std::process::{Command, Stdio};
+
+        let inc_path = &mq_path::mq_inc_path();
+
+        let supported_cfg = support::CFG_CHECKS
+            .iter()
+            .flat_map(|&(cfg, src)| support::check_compile(cfg, src, inc_path).map(|_| cfg));
+
+        for cfg in supported_cfg {
+            println!("cargo:rustc-cfg={cfg}");
+        }
+
+        println!("{}", &mq_path::mq_inc_path().display());
 
         // Generate and write the bindings file
         let out_path =
             std::path::PathBuf::from(std::env::var("OUT_DIR").map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?); // Mandatory OUT_DIR
         let out_bindings = out_path.join("bindings.rs");
 
-        let dspmqver_output = String::from_utf8(Command::new(mq_path::dspmqver()).stdout(Stdio::piped()).output()?.stdout)
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+        let dspmqver_path = mq_path::dspmqver();
+        let dspmqver_raw = Command::new(&dspmqver_path).stdout(Stdio::piped()).output()?;
+        let dspmqver_output =
+            String::from_utf8(dspmqver_raw.stdout).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
 
         let mq_version = Regex::new(r"(?m)^\s*Version:\s+(?<version>.*?)\s*$")
             .expect("valid regex")
