5b4d11a0ef184749f508beb9e04c369c4873c5f5
Warren Spits <warren@advantic.au>
2024-11-27 17:21:35 +1100

    workspace progress

--- libmqm-default/build/main.rs
+++ libmqm-default/build/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+use std::{fs::File, io::{self, Write}, mem::transmute};
+
+use libmqm_sys::lib;
+
+fn main() -> Result<(), io::Error> {
+    // Generate and write the serialised defaults
+    let out_path =
+        std::path::PathBuf::from(std::env::var("OUT_DIR").map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?); // Mandatory OUT_DIR
+
+    let defaults_path = out_path.join("defaults.rs");
+
+    let mqmd = lib::MQMD::default();
+    let mqmd_bytes = unsafe { transmute::<_, &[u8; lib::MQMD_CURRENT_LENGTH]>(&mqmd) };
     
+    let mut defaults_writer = io::BufWriter::new(File::create(&defaults_path)?);
+
+    for s_byte in mqmd_bytes {
+        write!(defaults_writer, "{}", s_byte)?;
+    }
+
+    Ok(())
 }
--- libmqm-sys/src/lib.rs
+++ libmqm-sys/src/lib.rs
@@ -1,13 +1,3 @@
-#![deny(clippy::all)]
-#![warn(clippy::nursery)]
-#![warn(clippy::pedantic)]
-#![warn(clippy::unwrap_used)]
-#![warn(clippy::unused_async)]
-#![warn(unsafe_op_in_unsafe_fn)]
-#![allow(clippy::similar_names)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
 /*!
 
 Bindings to the IBM® MQ Interface (MQI) and MQ Administration Interface (MQAI) C libraries.
