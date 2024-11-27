7f8785964d15c29019a7f80c78e8633d773f7f3c
Warren Spits <warren@advantic.au>
2024-11-28 08:48:25 +1100

    const default progress

--- libmqm-default/build/main.rs
+++ libmqm-default/build/main.rs
@@ -1,6 +1,36 @@
-use std::{fs::File, io::{self, Write}, mem::transmute};
+use std::{
+    fs::File,
+    io::{self, Write},
+    slice,
+};
 
 use libmqm_sys::lib;
+
+fn const_default<T: Default>(write: &mut impl Write, const_name: &str, type_name: &str) -> Result<(), io::Error> {
+    let dflt = T::default();
+    let s = slice::from_ref(&dflt);
+
+    let (prefix, bytes, suffix) = unsafe { s.align_to::<u8>() };
+    assert_eq!(prefix.len(), 0);
+    assert_eq!(suffix.len(), 0);
+    write!(
+        write,
+        "pub const {}: {} = unsafe {{ std::mem::transmute::<[u8; {}], _>([",
+        const_name,
+        type_name,
+        bytes.len()
+    )?;
+    for s_byte in bytes {
+        if s_byte.is_ascii_alphanumeric() || *s_byte == 0x20 {
+            write!(write, "b'{}',", *s_byte as char)?;
+        } else {
+            write!(write, "{s_byte:#02x},")?;
+        }
+    }
+
+    writeln!(write, "]) }};")?;
+    Ok(())
+}
 
 fn main() -> Result<(), io::Error> {
     // Generate and write the serialised defaults
@@ -9,14 +39,12 @@ fn main() -> Result<(), io::Error> {
 
     let defaults_path = out_path.join("defaults.rs");
 
-    let mqmd = lib::MQMD::default();
-    let mqmd_bytes = unsafe { transmute::<_, &[u8; lib::MQMD_CURRENT_LENGTH]>(&mqmd) };
-    
-    let mut defaults_writer = io::BufWriter::new(File::create(&defaults_path)?);
+    let mut w = io::BufWriter::new(File::create(&defaults_path)?);
 
-    for s_byte in mqmd_bytes {
-        write!(defaults_writer, "{}", s_byte)?;
-    }
+    const_default::<lib::MQCNO>(&mut w, "MQCNO_DEFAULT", "libmqm_sys::lib::MQCNO")?;
+    const_default::<lib::MQMD>(&mut w, "MQMD_DEFAULT", "libmqm_sys::lib::MQMD")?;
+    const_default::<lib::MQMD2>(&mut w, "MQMD2_DEFAULT", "libmqm_sys::lib::MQMD2")?;
+    const_default::<lib::MQGMO>(&mut w, "MQGMO_DEFAULT", "libmqm_sys::lib::MQGMO")?;
 
     Ok(())
 }
