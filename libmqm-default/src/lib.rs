3638509d2b7a94f54e70bb796de63fd5a80da7e2
Warren Spits <warren@advantic.au>
2024-12-01 10:56:02 +1100

    defaults pregen

--- libmqm-default/src/lib.rs
+++ libmqm-default/src/lib.rs
@@ -1,1 +1,7 @@
+#![cfg_attr(any(), rustfmt::skip)]
+
+#[cfg(feature = "defaultgen")]
 include!(concat!(env!("OUT_DIR"), "/defaults.rs"));
+
+#[cfg(not(feature = "defaultgen"))]
+include!("pregen.rs");
