dae5f81c15e229f58c907ba56b9046e526110561
Warren Spits <warren@advantic.au>
2024-12-02 09:16:17 +1100

    rustfmt ignore generated defaults

--- libmqm-default/src/lib.rs
+++ libmqm-default/src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(any(), rustfmt::skip)]
 
 #[cfg(feature = "defaultgen")]
+#[rustfmt::skip]
 mod defaults {
     include!(concat!(env!("OUT_DIR"), "/defaults.rs"));
 }
@@ -9,6 +10,7 @@ mod defaults {
 #[cfg_attr(all(target_os = "windows", target_arch = "x86_64"), path = "x86_64-windows-pregen.rs")]
 #[cfg_attr(all(target_os = "linux", target_arch = "x86_64"), path = "x86_64-linux-pregen.rs")]
 #[cfg_attr(target_os = "macos", path = "any-macos-pregen.rs")]
+#[rustfmt::skip]
 mod defaults;
 
 #[doc(inline)]
