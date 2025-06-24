8a03255d4709d728757207e8e44d34a584bc1127
Warren Spits <warren@advantic.au>
2025-06-24 22:27:01 +1000

    build: remove redundant allow rules

--- libmqm-sys/src/pregen/s390x-linux/mod.rs
+++ libmqm-sys/src/pregen/s390x-linux/mod.rs
@@ -1,9 +1,3 @@
-#![allow(clippy::all)]
-#![allow(clippy::pedantic)]
-#![allow(clippy::nursery)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
 include!("mqi.rs");
 #[cfg(feature = "exits")]
 include!("exits.rs");
